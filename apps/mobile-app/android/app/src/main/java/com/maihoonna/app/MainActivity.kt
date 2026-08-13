ae3521a71963db6d0a159ceb53f6f6940513313d
Harshit00018 <harshitshukla9305@gmail.com>
2026-08-13 20:00:11 +0530

    chore: update bundle identifier to com.maihoonna.app

--- apps/mobile-app/android/app/src/main/java/com/maihoonna/app/MainActivity.kt
+++ apps/mobile-app/android/app/src/main/java/com/maihoonna/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.rajeev_23.maihoonna
+package com.maihoonna.app
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- apps/mobile-app/android/app/src/main/java/com/maihoonna/app/MainApplication.kt
+++ apps/mobile-app/android/app/src/main/java/com/maihoonna/app/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.rajeev_23.maihoonna
+package com.maihoonna.app
 
 import android.app.Application
 import android.content.res.Configuration
